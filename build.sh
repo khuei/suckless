afb4dc2b38c8ddc790d1a476924d5f41f044d434
Khue Nguyen <Z5483Y@gmail.com>
2021-11-14 11:54:51 -0500

    refactor: combine scripts into build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,39 @@
-#!/bin/sh
+#!/bin/sh -e
 
-for dir in "dwm" "dmenu" "slock" "st" "slstatus"; do
-	echo "Building: $dir"
-	make -C "$dir" >/dev/null
-	echo "Done"
-done
+[ "$1" ] || echo 'Usage: ./build.sh [dwm] [st] [dmenu] [slock]' 1>&2
+
+clone() {
+	[ -d "$1" ] || git clone "$2/$1"
+	cd "$1"
+	git remote add upstream "git://git.suckless.org/$1"
+	git fetch upstream
+	git rebase upstream/master
+	cd "$START_PWD"
+}
+
+build() {
+	START_PWD=$(dirname "$0")
+	[ "$START_PWD" = . ] && START_PWD=$PWD
+	for name ; do
+		cd "$START_PWD"
+
+		sl=https://github.com/Z5483
+		case $name in
+			st) clone "$name" "$sl" ;;
+			dwm) clone "$name" "$sl" ;;
+			surf) clone "$name" "$sl" ;;
+			dmenu) clone "$name" "$sl" ;;
+			tabbed) clone "$name" "$sl" ;;
+			sent) clone "$name" "$sl" ;;
+			slock) clone "$name" "$sl" ;;
+			*) usage
+		esac
+
+		cd "$name"
+
+		make -s clean
+		make -s
+	done
+}
+
+build "$@"
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for dir in "dwm" "dmenu" "slock" "st" "slstatus"; do
-	echo "Installing: $dir"
-	make -C "$dir" install >/dev/null
-	echo "Done"
-done
